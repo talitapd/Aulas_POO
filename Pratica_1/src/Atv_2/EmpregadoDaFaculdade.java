2371483cbcd822f35235450c46add0483c499317
Talita Diniz <142948881+talitapd@users.noreply.github.com>
2023-09-14 18:34:28 -0300

    Add files via upload

---
+++ Pratica_1/src/Atv_2/EmpregadoDaFaculdade.java
@@ -0,0 +1,29 @@
+package Atv_2;
+
+public class EmpregadoDaFaculdade {
+    private String nome;
+    private double salario;
+    private int horasAula;
+
+    public double getGastos() {
+        double gastos = this.salario + (horasAula * 40);
+        return gastos;
+    }
+
+    public String getInfo() {
+        double valorTotal = this.salario + (horasAula * 40);
+        return "Nome: " + this.nome + " receberá: " + valorTotal;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void setHorasAula(int horasAula) {
+        this.horasAula = horasAula;
+    }
+}
