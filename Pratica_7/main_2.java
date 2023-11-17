a4b01c0b4a3cca265be7cfd3cf610083aedd1ecd
Talita Diniz <142948881+talitapd@users.noreply.github.com>
2023-11-16 21:19:12 -0300

    Update main_2.java

--- Pratica_7/main_2.java
+++ Pratica_7/main_2.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 
 public class main_2 {
     public static void main(String[] args) throws SQLException {
-        //inserir dados
+       
         String id_isbn, nm_titulo,cod_excluir,buscaTitulo ;
         int id_categoria, id_editora;
         Double vl_preco, buscaValor;
@@ -17,17 +17,13 @@ public class main_2 {
         vl_preco =  Double.parseDouble(JOptionPane.showInputDialog("Digite preco do livro"));
         Inserir inserir = new Inserir(id_isbn,id_categoria, id_editora, nm_titulo, vl_preco);
 
-        //Exclduir
         cod_excluir = JOptionPane.showInputDialog("Digite ISBN para Excluir");
         Excluir excluir = new Excluir(cod_excluir);
 
-
-        //Consulta titulo
         Consulta busca = new Consulta();
         buscaTitulo = JOptionPane.showInputDialog("Digite Titutlo para buscar");
         busca.BuscaTitulo(buscaTitulo);
 
-        //Consulta por valor
         buscaValor =Double.parseDouble( JOptionPane.showInputDialog("Digite Titutlo para buscar"));
         busca.BuscaValor(buscaValor);
 
